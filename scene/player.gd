c02f718be5f4cf13ac48c1d52790133593305744
Atharv-Shukla-987 <249597135+Atharv-Shukla-987@users.noreply.github.com>
2026-05-22 21:44:59 +0530

    added hitbox and some code fix and added attack sound effect

--- scene/player.gd
+++ scene/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var sword_swing: AudioStreamPlayer2D = $"sword swing"
+@onready var hitbox: Area2D = $hitbox
 
 
 const SPEED = 300.0
 var lastdirection : Vector2 =  Vector2.RIGHT
 var isattacking: bool = false
+var hitboxoffset : Vector2
+func _ready() -> void:
+	hitboxoffset = hitbox.position
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack") and not isattacking:
 		
@@ -33,6 +38,7 @@ func process_movement() -> void:
 	if direction!= Vector2.ZERO:
 		velocity= direction*SPEED
 		lastdirection= direction
+		update_hiboxoffset()
 	else:
 		velocity = Vector2.ZERO
   
@@ -68,3 +74,21 @@ func attack() -> void:
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if isattacking:
 		isattacking = false
+		
+		
+		
+		
+#hitbox offset
+func update_hiboxoffset() -> void:
+	var x = hitboxoffset.x
+	var y = hitboxoffset.y 
+	match lastdirection:
+		Vector2.LEFT :
+			hitbox.position = Vector2(-x,y)
+		Vector2.RIGHT :
+			hitbox.position = Vector2(x,-y)
+		Vector2.UP :
+			hitbox.position == Vector2(y,-x)
+		Vector2.DOWN:
+			hitbox.position = Vector2(-y,x)
+	
